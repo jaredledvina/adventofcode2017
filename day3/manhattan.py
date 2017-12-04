ea3d3346581d17fee2c3c9b693a301ac6a30ebf2
Jared Ledvina <jared@techsmix.net>
2017-12-03 21:50:26 -0500

    Day 3 part 2 complete, and this code is total shit

--- day3/manhattan.py
+++ day3/manhattan.py
@@ -1,4 +1,6 @@
 #! /usr/bin/env python
+from collections import defaultdict
+
 
 def get_position(distance):
     """
@@ -25,11 +27,11 @@ def get_position(distance):
     for step in range(1, distance):
         if direction == right:
             x += 1
-        if direction == up:
+        elif direction == up:
             y += 1
-        if direction == left:
+        elif direction == left:
             x -= 1
-        if direction == down:
+        elif direction == down:
             y -= 1
 
         steps += 1
@@ -58,5 +60,69 @@ def check_distance(distance):
     return abs(x) + abs(y)
 
 
+def squared_square(maximum):
+    """
+    >>> squared_square(4)
+    2
+    >>> squared_square(147)
+    142
+    >>> squared_square(806)
+    747
+    """
+    # Directions
+    right = 0
+    up = 1
+    left = 2
+    down = 3
+    # Start
+    x = 0
+    y = 0
+    direction = 0 # Start going right
+    direction_change = 0
+    steps = 0
+    max_steps = 1
+
+    # State
+    location_bits = {
+        (0, 0): 1
+    }
+
+    previous_sum = 1
+    while previous_sum < maximum:
+        previous_location = (x, y)
+        if direction == right:
+            x += 1
+        elif direction == up:
+            y += 1
+        elif direction == left:
+            x -= 1
+        elif direction == down:
+            y -= 1
+
+        steps += 1
+        new_sum = 0
+        new_sum += location_bits.get((x - 1, y), 0)
+        new_sum += location_bits.get((x + 1, y), 0 )
+        new_sum += location_bits.get((x , y - 1), 0)
+        new_sum += location_bits.get((x , y + 1), 0)
+        new_sum += location_bits.get((x + 1 , y + 1), 0)
+        new_sum += location_bits.get((x + 1 , y - 1), 0)
+        new_sum += location_bits.get((x - 1 , y + 1), 0)
+        new_sum += location_bits.get((x - 1 , y - 1), 0)
+        print('Sum for:', x, y, 'is', new_sum)
+        location_bits[(x, y)] = new_sum
+        previous_sum = location_bits[previous_location]
+        if steps == max_steps:
+            steps = 0
+            direction_change += 1
+            direction = (direction + 1) % 4
+            # Max steps increases by one on evens
+            if direction_change % 2 == 0:
+                max_steps += 1
+    return previous_sum
+
+
+
 if __name__ == '__main__':
     print('The distance is:', check_distance(265149))
+    print('The previous number was:', squared_square(265149))
